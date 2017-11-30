cf372dcdaa1595402290ea293fd17307ad695134
Thiago Silva Marra <thiago.marra@simconsultas.com.br>
2017-11-29 22:09:43 -0200

    added comicBookRepository.

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -11,21 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                    SeriesTitle = "The Amazing Spider-Man",
-                    IssueNumber = 700,
-                    DescriptionHTML = "<p><strong>will Peter Parker?</strong></p>",
-                    Artists = new Artist[]
-                    {
-                       new Artist(){ Name= "Script Role", Role =  "Dan Slott" },
-                      new Artist(){ Name=   "Pencils" ,Role= "Humberto Ramos" },
-                      new Artist(){ Name=   "Inks" ,Role= "Victor Olazaba" },
-                      new Artist(){ Name=   "Colors" ,Role= "Edgar Delgado" },
-                      new Artist(){ Name=   "Letters",Role= "Chris Eliopoulos" }
-                    }
-            };
-            return View(comicBook);
+            return View();
         }
     }
 }
